a5f3b1edd63cb76058af077135b3c4954b2826d6
David W. Brown <davidwbrown@hey.com>
2025-04-12 21:34:36 -0500

    Initial Commit

---
+++ MultipeerBench/MultipeerBenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipeerBenchApp.swift
+//  MultipeerBench
+//
+//  Created by David Brown on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MultipeerBenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
